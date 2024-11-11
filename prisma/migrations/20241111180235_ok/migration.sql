82c65a5d4c33f214410c8f8a148f6c5c721768d5
Abu Sayed <abusayed.in22@gmail.com>
2024-11-12 00:03:10 +0600

    hello

---
+++ prisma/migrations/20241111180235_ok/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `duelist` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `duelist` DROP COLUMN `name`;
