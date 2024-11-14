c80f90555a81fc3b89426fb0756c814cd373fad1
Abu Sayed <abusayed.in22@gmail.com>
2024-11-14 18:47:51 +0600

    ok

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "dist", // Custom output folder
+  // distDir: "dist", // Custom output folder  -----it's not working when deploy on vercel
 //   output: "standalone", // Enable standalone mode for serverless deployment
 };
 
